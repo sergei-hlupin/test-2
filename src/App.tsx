2ef1d034bc2afe67c08d3e9ed9884d8dd6ba80b9
Ilya Malashenko <ilya.malashenko.developer@gmail.com>
2023-08-13 20:13:19 +0300

    Инициализация базового проекта

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from 'stories';
+import { Typography } from '@mui/material';
+
+const App = () => {
+
+  return (
+    <>
+      <Button label='hello world' />
+      <Typography >hello world</Typography>
+    </>
+  )
+}
+
+export default App
